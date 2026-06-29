a857b47eaa94c600b9dcb8316112e0bd6d3311be
Breeze <akshadsatpute1989@gmail.com>
2026-06-29 17:08:12 +0530

    anisearch

---
+++ ronova/plugins/user/ani_search.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, ReplyParameters
+
+from config import PREFIXES, ADMIN_ID, BOT
+
+
+@Client.on_message(filters.command("anisearch", prefixes=PREFIXES) & filters.user(ADMIN_ID))
+async def get_ani(c: Client, m: Message):
+    if len(m.command) < 2:
+        await m.reply("Usage: <code>.anisearch &lt;anime name&gt;</code>")
+        return
+
+    name    = " ".join(m.command[1:])
+    results = await c.get_inline_bot_results(bot=BOT, query=f"anime {name}")
+
+    await c.send_inline_bot_result(
+        chat_id=m.chat.id,
+        query_id=results.query_id,
+        result_id=results.results[0].id,
+        reply_parameters=ReplyParameters(message_id=m.id)
+    )
--- ronova/plugins/utilities/__init__.py
+++ ronova/plugins/utilities/__init__.py
@@ -1,3 +1,4 @@
 from .get_bin import paste, delete_paste
 from .dev_utilities import eval_helper, get_output
 from .think_utility import AiSearch
+from .getani import fetch_anime
