7429d770e7292afa65852862a6a570b1ecb56d1c
Breeze <akshadsatpute1989@gmail.com>
2026-07-11 18:34:33 +0530

    added all plugins in init

---
+++ ronova/plugins/user/__init__.py
@@ -0,0 +1,9 @@
+from . import (ani_search, delete, eval, just_google_it,
+               logs, search_movie, search_word, shell,
+               start, stats, think, wiki)
+
+__all__ :list[str] = [
+    "ani_search", "delete", "eval", "just_google_it",
+    "logs", "search_movie", "search_word", "shell",
+    "start", "stats", "think", "wiki"
+]
