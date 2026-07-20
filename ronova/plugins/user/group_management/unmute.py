61d05e0ef0d33d8cce5d6e0a2e55f6390cf7edc8
Breeze <akshadsatpute1989@gmail.com>
2026-07-20 21:12:19 +0530

    added unmute

---
+++ ronova/plugins/user/group_management/unmute.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+from typing import Optional
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, ChatPermissions
+
+from config import PREFIXES, ADMIN_ID
+from .utils import RetriveData
+
+COMMAND_UNBANS: list[str] = ["unmute"]
+
+async def mute_user(
+    c: Client,
+    m: Message,
+    chat_id: int,
+    target_id: int,
+    time: Optional[datetime]
+):
+    try:
+        await c.restrict_chat_member(
+            chat_id=chat_id,
+            user_id=target_id,
+            until_date=time,
+            permissions=ChatPermissions(
+                can_send_messages = True,
+                can_send_audios = True,
+                can_send_documents = True,
+                can_send_photos = True,
+                can_send_videos = True,
+                can_send_video_notes = True,
+                can_send_voice_notes = True,
+                can_send_polls = True,
+                can_send_other_messages = True,
+                can_add_web_page_previews = True,
+                can_react_to_messages = True,
+                can_edit_tag = True,
+                can_change_info = True,
+                can_invite_users = True,
+                can_pin_messages = True,
+                can_manage_topics = True)
+        )
+        return True
+    
+    except Exception as e:
+        await m.reply_text(f"Mute failed:\n{e}")
+        return False
+
+@Client.on_message(
+    filters.command(COMMAND_UNBANS, prefixes=PREFIXES)
+    & filters.user(ADMIN_ID)
+    & filters.group
+)
+async def gc_mang(c: Client, m: Message):
+
+    parser = RetriveData(c, m)
+    data = await parser.unban_data()
+
+    chat_id = data["chat_id"]
+    target_id = data["target_id"]
+
+    if not target_id or target_id == m.from_user.id:
+        return
+
+    try:
+        await c.restrict_chat_member(
+            chat_id=chat_id,
+            user_id=target_id,
+            permissions=ChatPermissions(
+                can_send_messages = True,
+                can_send_audios = True,
+                can_send_documents = True,
+                can_send_photos = True,
+                can_send_videos = True,
+                can_send_video_notes = True,
+                can_send_voice_notes = True,
+                can_send_polls = True,
+                can_send_other_messages = True,
+                can_add_web_page_previews = True,
+                can_react_to_messages = True,
+                can_edit_tag = True,
+                can_change_info = True,
+                can_invite_users = True,
+                can_pin_messages = True,
+                can_manage_topics = True)
+        )
+        await m.reply_text(f"unmuted user `{target_id}`")
+    except Exception as e:
+        await m.edit(e)
