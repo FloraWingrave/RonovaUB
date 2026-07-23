a1bf3985e6cb5150fa276a32e53dbac5cda9e90c
Flora Wingrave <102516901+FloraWingrave@users.noreply.github.com>
2026-07-23 15:39:54 +0530

    Update data.py

--- ronova/plugins/shared/data.py
+++ ronova/plugins/shared/data.py
@@ -1,35 +1,6 @@
 from dataclasses import dataclass, field
 
 @dataclass
-class AfkState:
-    status: bool = False
-    reason: str | None = None
-    afk_time: float | None = None
-    file_type: str | None = None
-    file_id: str | None = None
-    media_from_chat: int | None = None
-    message_media_id: int | None = None
-    users: list[int] = field(default_factory=list)
-
-AFK_DATA = AfkState()
-
-@dataclass
-class MusicState:
-    status: bool = False
-    user_chat_id:int | None = None
-    user_message_id:int | None = None 
-
-MUSIC_STATE = MusicState()
-
-@dataclass
-class QuoteState:
-    status: bool = False
-    user_chat_id:int | None = None
-    user_message_id:int | None = None
-
-QUOTE_STATE =  QuoteState()
-
-@dataclass
 class PremiumState:
     status: bool = False
     text: str | None = None
